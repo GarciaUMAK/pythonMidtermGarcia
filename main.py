88433884f11b7929927faafbbbf2a3a7b493c9f1
KG <kgarcia.k12257313@umak.edu.ph>
2026-09-24 20:20:10 +0800

    1st commit

---
+++ main.py
@@ -0,0 +1,34 @@
+def calculation(q, ppu):
+    totalAmmount = q * ppu
+
+    print(f"Item Name: {itemName}, total Ammount: {totalAmmount}, Price Per Unit: {ppu}")
+
+def exitNa():
+    exit()
+itemName = ""
+quantitySold = ""
+pricePerUnit = ""
+
+lop = True
+
+while lop:
+
+    print("========================================")
+    print("SALES RECORD MANAGEMENT SYSTEM")
+    print("========================================")
+    u = input("Select an option (1-4): ")
+
+    if u == "1":
+        try:
+            itemName = input("Item name: ")
+            quantitySold = input("Quantity sold: ")
+            quantitySold = int(quantitySold)
+            pricePerUnit = input("Price per unit: ")
+            pricePerUnit = float(pricePerUnit)
+            calculation(quantitySold, pricePerUnit)
+        except ValueError:
+            print("Error! Please input proper value.")
+
+    if u == "4":
+        print("Thank you for using the Sales Record Management System.")
+        exitNa()
